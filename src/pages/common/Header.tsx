c1934a3f1bee06e4d41ee801ccae394de03800ad
snowmemorySM <74525479+snowmemorySM@users.noreply.github.com>
2026-06-09 16:35:33 +0900

    20260609 에러수정

--- src/pages/common/Header.tsx
+++ src/pages/common/Header.tsx
@@ -191,7 +191,7 @@ function Header() {
                   </div>
                 ),
               )}
-              {/* {languageDropdown} */}
+              {languageDropdown}
             </Nav>
 
             <div className="site-mobile-menu">
@@ -231,7 +231,7 @@ function Header() {
                   ) : null,
                 )}
               </Accordion>
-              {/* <div className="mobile-language-dropdown">{languageDropdown}</div> */}
+              <div className="mobile-language-dropdown">{languageDropdown}</div>
             </div>
           </Navbar.Collapse>
         </Container>
